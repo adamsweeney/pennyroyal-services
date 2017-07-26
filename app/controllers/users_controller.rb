4f2a149c93aa17c3812dfae31bcf50270dd568ab
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-07-26 17:12:22 -0400

    Add some basic user stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
 
 	def create
-
+		@user = User.create(user_params)
+		@user.save
+		respond_with @user
 	end
 
 	def show
-
+		respond_with User.find(params[:id])
 	end
 
 	def index
 
 	end
+
+	private
+
+	def user_params
+      params.permit(:first_name, :last_name, :username)
+  end
+
 end
