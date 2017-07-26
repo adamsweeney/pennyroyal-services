e8422a3b15dc33962696c47f429afc915d4ee9c1
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-07-26 14:38:10 -0400

    Add stock table

--- db/migrate/20170726164038_stocks.rb
+++ db/migrate/20170726164038_stocks.rb
@@ -1,4 +1,14 @@
 class Stocks < ActiveRecord::Migration
   def change
+    create_table :stocks do |s|
+      s.integer :user
+      s.string :code
+      s.string :name
+      s.string :market
+      s.decimal :buying_price
+      s.decimal :current_price
+      s.date :bought_at
+      s.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726162542) do
+ActiveRecord::Schema.define(version: 20170726164038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.integer  "user"
+    t.string   "code"
+    t.string   "name"
+    t.string   "market"
+    t.decimal  "buying_price"
+    t.decimal  "current_price"
+    t.date     "bought_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
