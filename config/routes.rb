026e21a8ca70d7057b71c136c607794e4ee1f1a0
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-07-26 15:15:29 -0400

    Add root to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 	resources :users, only: [:show, :index, :create]
+	root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
