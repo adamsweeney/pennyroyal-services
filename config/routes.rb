9801facaf47b025fe993dddcee24b62f83854578
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-07-30 18:08:48 -0400

    API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-	resources :users, only: [:show, :create]
-	resources :stocks, only: [:create, :show, :index, :delete]
-	root 'pages#home'
+	namespace :api, { defaults: { format: :json } } do
+		resources :users, only: [:show, :create]
+		resources :stocks, only: [:create, :show, :index, :delete]
+		root 'pages#home'
+	end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
